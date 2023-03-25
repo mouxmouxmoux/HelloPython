8de9f8cabf75426cb3db23e2a48dd48dc0ea90ff
bwyun <bwuyun@alu.fudan.edu.cn>
2023-03-25 09:34:15 +0800

    Changes

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    g = 7
+    print("请输入一个年龄。")
+    year = int(input())
+    if year > g:
+        print("大")
+    if year == g:
+        print("一样大")
+    if year < g:
+        print("小")
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
